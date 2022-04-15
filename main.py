fb53f58adb029adaa36e9446e43d13eec4cc9d20
nitabooo <alanbiju154@gmail.com>
2022-04-15 09:13:12 +0530

    added buttons

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+from tkinter import *
+from tkinter.ttk import *
+  
+root = Tk()
+root.geometry("800x500")
+   
+w = Label(root, text ='GeeksForGeeks',
+          font = "50") 
+
+photo1 = PhotoImage(file = r"C:\Users\Alan\Desktop\twitter.png")
+photo2 = PhotoImage(file = r"C:\Users\Alan\Desktop\dead-removebg-preview.png") 
+# Resizing image to fit on button
+photoimage1 = photo1.subsample(20,20)
+photoimage2 = photo2.subsample(15,15)
+Button(root, text = 'Click Me !', image = photoimage1,
+                    compound = LEFT).pack(side = RIGHT)
+Button(root, text = 'Click Me not !', image = photoimage2,
+                    compound = LEFT).pack(side = RIGHT)
+   
+w.pack()
+   
+
+   
+root.mainloop()
